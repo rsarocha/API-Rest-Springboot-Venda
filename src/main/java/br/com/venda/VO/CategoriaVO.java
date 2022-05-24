7c7322393c6d4402c8ade68db14d0eb874ace272
Rodrigo Rocha <rsarocha86@gmail.com>
2022-05-24 16:14:22 -0300

    refatorando VO

--- src/main/java/br/com/venda/VO/CategoriaVO.java
+++ src/main/java/br/com/venda/VO/CategoriaVO.java
@@ -1,27 +1,34 @@
 package br.com.venda.VO;
 
+import java.util.List;
+
+import br.com.venda.entity.Produto;
+
 public class CategoriaVO {
 
 	
 	private Long codigo;
 	private String nome;
-
-	
-
+	private List<Produto> produto;
 	public Long getCodigo() {
 		return codigo;
 	}
-
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
 
 }
