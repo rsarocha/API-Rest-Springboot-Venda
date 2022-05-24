3fd261b89a10e5cbd2707084ec8b85cfe14d3477
Rodrigo Rocha <rsarocha86@gmail.com>
2022-05-24 14:31:11 -0300

    implementando a camada Repository

---
+++ src/main/java/br/com/venda/repository/CategoriaRepository.java
@@ -0,0 +1,15 @@
+package br.com.venda.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.venda.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	Optional<Categoria> findByNome(String nome);
+
+	Optional<Categoria> findByCodigo(Long codigo);
+
+}
